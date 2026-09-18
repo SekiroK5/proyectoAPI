d3d03c8a142d87ba2bf20ccb5bfa528770534bd4
SekiroK5 <arin01246@gmail.com>
2026-09-18 12:35:55 -0600

    feat(dto): configurar exclusion de campos nulos en respuestas JSON y actualizar DTO de productos

--- src/main/java/com/proyecto/servicios/model/producto/ProductoDto.java
+++ src/main/java/com/proyecto/servicios/model/producto/ProductoDto.java
@@ -1,10 +1,12 @@
 package com.proyecto.servicios.model.producto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ProductoDto {
 
     private Integer idServicio;
